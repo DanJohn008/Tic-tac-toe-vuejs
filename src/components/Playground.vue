90f4e9afa45121ad3445e6a35194a120bef3b1cb
DanielJ <DanielJ@miway.co.za>
2018-02-02 09:03:27 +0200

    Add new basic component

---
+++ src/components/Playground.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="board">
+        <div>{{ testmsg }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PlaygroundBoard',
+    data () {
+        return {
+            testmsg: 'Test message'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.board {
+    margin: 10em;
+}
+
+</style>
