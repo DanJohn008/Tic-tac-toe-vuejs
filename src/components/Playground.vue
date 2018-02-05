f97b3914a59cc052f17f862c6826de7bdd0f2929
DanielJ <DanielJ@miway.co.za>
2018-02-05 08:24:02 +0200

    Build better layout

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,51 +1,70 @@
 <template>
-    <v-layout>
-        <v-flex xs12 sm6 offset-sm3>
-            <v-card>
-                <v-card-actions>
-                    <v-select label="Size" :items="items" v-model="size"></v-select>
-                    <v-spacer></v-spacer>
-                </v-card-actions>
-                <v-container fluid v-bind="{ [`grid-list-${size}`]: true }">
-                    <v-layout row wrap>
-                        <v-flex
-                                xs4
-                                v-for="n in 9"
-                                :key="n"
-                        >
-                            <v-card flat tile>
-                                <v-card-media
-                                        :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
-                                        height="150px"
-                                >
-                                </v-card-media>
-                            </v-card>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
-            </v-card>
-        </v-flex>
-    </v-layout>
+    <v-container grid>
+        <v-layout row>
+            <v-flex md4>
+                <v-card dark tile flat>
+                    <v-card-text>A1</v-card-text>
+                </v-card>
+            </v-flex>
+            <v-flex md4>
+                <v-card dark tile flat>
+                    <v-card-text>A2</v-card-text>
+                </v-card>
+            </v-flex>
+            <v-flex md4>
+                <v-card dark tile flat>
+                    <v-card-text>A3</v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+        <v-layout row>
+            <v-flex md4>
+                <v-card dark tile flat>
+                    <v-card-text>B1</v-card-text>
+                </v-card>
+            </v-flex>
+            <v-flex md4>
+                <v-card dark tile flat>
+                    <v-card-text>B2</v-card-text>
+                </v-card>
+            </v-flex>
+            <v-flex md4>
+                <v-card dark tile flat>
+                    <v-card-text>B3</v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+        <v-layout row>
+            <v-flex md4>
+                <v-card dark tile flat>
+                    <v-card-text>C1</v-card-text>
+                </v-card>
+            </v-flex>
+            <v-flex md4>
+                <v-card dark tile flat>
+                    <v-card-text>C2</v-card-text>
+                </v-card>
+            </v-flex>
+            <v-flex md4>
+                <v-card dark tile flat>
+                    <v-card-text>C3</v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
     export default {
         data: () => ({
-            size: 'sm',
-            items: [
-                { text: 'Extra small (1px)', value: 'xs' },
-                { text: 'Small (4px)', value: 'sm' },
-                { text: 'Medium (8px)', value: 'md' },
-                { text: 'Large (16px)', value: 'lg' },
-                { text: 'Extra large (24px)', value: 'xl' }
-            ]
+            msg: 'test'
         })
     }
 </script>
 
 <style scoped>
-.board {
-    margin: 10em;
+.container {
+    max-width: 580px;
 }
 
 </style>
