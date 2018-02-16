7418a6926a3110371cfc4926e938e334e2fb04e2
DanielJ <DanielJ@miway.co.za>
2018-02-16 07:21:00 +0200

    Fix styling

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,53 +1,53 @@
 <template>
     <v-container grid>
         <v-layout row>
-            <v-flex md4>
+            <v-flex xs4>
                 <v-card dark tile flat>
                     <v-card-text>A1</v-card-text>
                 </v-card>
             </v-flex>
-            <v-flex md4>
+            <v-flex xs4>
                 <v-card dark tile flat>
                     <v-card-text>A2</v-card-text>
                 </v-card>
             </v-flex>
-            <v-flex md4>
+            <v-flex xs4>
                 <v-card dark tile flat>
                     <v-card-text>A3</v-card-text>
                 </v-card>
             </v-flex>
         </v-layout>
         <v-layout row>
-            <v-flex md4>
+            <v-flex xs4>
                 <v-card dark tile flat>
                     <v-card-text>B1</v-card-text>
                 </v-card>
             </v-flex>
-            <v-flex md4>
+            <v-flex xs4>
                 <v-card dark tile flat>
                     <v-card-text>B2</v-card-text>
                 </v-card>
             </v-flex>
-            <v-flex md4>
+            <v-flex xs4>
                 <v-card dark tile flat>
                     <v-card-text>B3</v-card-text>
                 </v-card>
             </v-flex>
         </v-layout>
         <v-layout row>
-            <v-flex md4>
+            <v-flex xs4>
                 <v-card dark tile flat>
                     <v-card-text>C1</v-card-text>
                 </v-card>
             </v-flex>
-            <v-flex md4>
+            <v-flex xs4>
                 <v-card dark tile flat>
                     <v-card-text>C2</v-card-text>
                 </v-card>
             </v-flex>
-            <v-flex md4>
-                <v-card dark tile flat>
-                    <v-card-text>C3</v-card-text>
+            <v-flex xs4>
+                <v-card dark>
+                    <v-card-text>C3 </v-card-text>
                 </v-card>
             </v-flex>
         </v-layout>
